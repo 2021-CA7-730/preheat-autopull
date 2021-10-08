0d749f6943347f129c48e57ac5697d4d5fdfacec
Jan Peter <44668286+JanPeterChristiansen@users.noreply.github.com>
2021-10-08 14:59:43 +0200

    Update preheat_autopull.py

--- preheat_autopull.py
+++ preheat_autopull.py
@@ -7,8 +7,7 @@ Created on Fri Oct  8 14:14:56 2021
 import preheat_open as ph
 import TimeKeeper
 TK = TimeKeeper.TimeKeeper()
-# %%
-
+# %% monkey patch logging functions to mute the console output
 VERBOSE = False
 
 
@@ -19,7 +18,25 @@ def info(self, msg: str, *args, **kwargs) -> None:
 
 ph.logging.Logging.info = info
 
-# %% hent building
-
+# %%
 b_id = 2245
 b = ph.Building(b_id)
+
+delay = 60*60*1  # time delay in seconds
+start_date = TK.get_now_local_delay(-delay)
+end_date = TK.get_now_local()
+res = "raw"
+
+b.load_data(start_date, end_date, res)
+
+
+# make list of sensors and actuators
+sensor_names = []
+for b_unit in b.units.values():
+    for unit in b_unit:
+        if unit.components:
+            for component in unit.components:
+                sensor_names.append(component.tag)
+        if unit.sub_units:
+            for sub_unit in unit.sub_units:
+                sensor_names.append(sub_unit.name)
