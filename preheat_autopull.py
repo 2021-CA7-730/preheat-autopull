08215ed49b5c0e0381c7d330644a06c50c72cf85
Jan Peter <44668286+JanPeterChristiansen@users.noreply.github.com>
2021-10-11 12:27:09 +0200

    added more resolutions

--- preheat_autopull.py
+++ preheat_autopull.py
@@ -4,40 +4,75 @@ Created on Fri Oct  8 14:14:56 2021
 
 @author: Jan Peter Mortensen Christiansen
 """
+import os
+import pandas as pd
 import preheat_open as ph
 import TimeKeeper
-import json
-TK = TimeKeeper.TimeKeeper()
-ph.logging.set_logging_level("warning")
+TK = TimeKeeper.TimeKeeper()  # make TimeKeeper object to handle datetimes and schedules
+ph.logging.set_logging_level("warning")  # set logging level to warning
 
+# %% function definitions
+
+
+def get_data_from_building_object(b, start_date, end_date, res):
+    b.clear_data()  # clear old data
+    b.load_data(start_date, end_date, res)  # load data for all sensors in building
+
+    # make list of sensors and data
+    sensor_names = []
+    sensor_data = []
+
+    # get all sensor names and data
+    for b_unit in b.units.values():  # check all building units
+        for unit in b_unit:  # check all units in building unit
+            if unit.has_data():  # check if units has any data in components
+                for component in unit.components:  # check all components in unit
+                    # append the name of the sensor
+                    sensor_names.append(component.tag)
+                    # append the data from the sensor
+                    sensor_data.append(unit.data[component.name].to_frame())
+            if unit.sub_units:  # check if unit has sub units
+                for sub_unit in unit.sub_units:  # check all sub units in unit
+                    if sub_unit.has_data():  # check if sub unit has data
+                        # append the name of the sensor
+                        sensor_names.append(sub_unit.name)
+                        # append the data from the sensor
+                        sensor_data.append(sub_unit.data)
+
+    return [sensor_names, sensor_data]
+
+# %%
+
+
+# set up building
 b_id = 2245
 b = ph.Building(b_id)
 
+
+# make data directory for today
+dirname = TK.get_now_local().strftime("%Y-%m-%d")
+if not(os.path.isdir(dirname)):
+    os.mkdir(dirname)
+    print("Directory created: "+dirname+"!")
+
+
+# make datetimes for data interval
 delay = 60*60*24  # time delay in seconds
 start_date = TK.get_now_local_delay(-delay)  # start time of the data query
 end_date = TK.get_now_local()  # end/stop time of the data query
-res = "raw"
+resolutions = ["raw", "minute", "hour"]  # resolutions to queue
 
-b.load_data(start_date, end_date, res)  # load data for all sensors in building
+for res in resolutions:
+    # get data for given res
+    [sensor_names, sensor_data] = get_data_from_building_object(b, start_date, end_date, res)
 
+    # make dataframe for saving
+    data = pd.DataFrame()
+    for (name, sensor) in zip(sensor_names, sensor_data):
+        data[name] = sensor
 
-# make list of sensors and data
-sensor_names = []
-sensor_data = []
-
-# get all sensor names and data
-for b_unit in b.units.values():  # check all building units
-    for unit in b_unit:  # check all units in building unit
-        if unit.has_data():  # check if units has any data in components
-            for component in unit.components:  # check all components in unit
-                # append the name of the sensor
-                sensor_names.append(component.tag)
-                # append the data from the sensor
-                sensor_data.append(unit.data[component.name].to_frame())
-        if unit.sub_units:  # check if unit has sub units
-            for sub_unit in unit.sub_units:  # check all sub units in unit
-                if sub_unit.has_data():  # check if sub unit has data
-                    # append the name of the sensor
-                    sensor_names.append(sub_unit.name)
-                    # append the data from the sensor
-                    sensor_data.append(sub_unit.data)
+    # save resolution data
+    filename = dirname+"/"+res+".csv"
+    if not(os.path.isfile(filename)):
+        data.to_csv(filename)
+        print("file saved: "+filename+"!")
